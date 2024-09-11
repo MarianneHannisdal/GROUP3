b8f965c8bb68f39624fea22a0bd0c0798cfe5b5a
MarianneHannisdal <marianne@hjellvik.com>
2024-09-11 15:06:50 +0200

    Arranging the code in a pipe. New full joined dataset with adjustments

--- SCRIPTS/Tidy_Adjust_Explore_Main_Script.R
+++ SCRIPTS/Tidy_Adjust_Explore_Main_Script.R
@@ -21,39 +21,7 @@ MyData<-read.delim(here("DATA","tidy_exam_dataset.txt"))
 # Getting an overview
 skimr::skim(MyData)
 
-# Removing unnecessary columns from the dataset
-MyData <- MyData %>%
-  select(-AA, -bGS, -BN., -OrganConfined)
-
-skimr::skim(MyData)
-
-# Make necessary changes in variable types
-MyData <- MyData %>%
-  mutate (subject = as.numeric(subject))
-class (MyData$subject)
-
-# Arrange ID column in order of increasing number or alphabetically
-MyData <- MyData %>%
-  arrange(subject)
-
-# Create a column showing whether `PVol` is higher than 100 or not: values High/Low
-MyData <- MyData %>%
-  mutate(VolumeHighOrLow = ifelse(PVol > 100, "High", "Low"))
-
-# Create a column showing `recurrence` as Yes/No
-MyData <- MyData %>%
-  mutate(recurrence = ifelse(Recurrence == 0, "No", "Yes"))
-
-# Create a numeric column showing multiplication of `AnyAdjTherapy` and `PreopTherapy` for each person
-MyData <- MyData %>%
-  mutate(TotalTherapy = AnyAdjTherapy * PreopTherapy)
-
-# Set the order of columns as: `id, hospital, Age` and other columns
-MyData <- MyData %>%
-  select(subject, Hospital, Age, everything())
-View(MyData)
-
-# Organize the above code as a pipe
+# Rearrange the code, in the format of a pipe
 MyDataNew <- MyData %>%
   select(-AA, -bGS, -BN., -OrganConfined) %>%
   mutate (subject = as.numeric(subject)) %>%
@@ -64,30 +32,22 @@ MyDataNew <- MyData %>%
   select(subject, Hospital, Age, everything())
 
 
-
 # Joining datasets ----
-# - Read and join the additional dataset to your main dataset.
-MyData2<-read.delim(here("DATA","exam_joindata.txt"))
-
-View(MyData2)
-skimr::skim(MyData)
-skimr::skim(MyData2)
-
-# We want to join the data by ID, but the ID is named "subject" in MyData, and "id" in MyData2
-# change the dame of id to subject in MyData2
-MyData2 <- MyData2 %>%
+# Read and join the additional dataset to your main dataset.
+# Organizing in a pipe
+MyData2<-read.delim(here("DATA","exam_joindata.txt")) %>%
   rename(subject = id)
 
 # Now we can join by matching "subject". MyData2 has 200 rows and MyData has 316
 # Perform a full join by "subject"
 full_data <- MyData %>%
   full_join(MyData2, by = "subject")
-View(full_data)
 
 # looks good, so we overwrite full_data to MyData
 MyData <- full_data
-View(MyData)
 
-# Organizing in a pipe
-MyData2<-read.delim(here("DATA","exam_joindata.txt")) %>%
-  rename(subject = id)
+# Saving the dataset with a Tidy name
+fileName <- paste0("tidy_adjust_exam_dataset", ".txt")
+write_delim(MyData, 
+            file = here("DATA", fileName), delim="\t")
+
